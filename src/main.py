c450c39db57cfa25a6a1c670b6c9b8dfbf1302d7
maverick <mahambrey.saurabh07@gmail.com>
2023-06-10 14:41:29 +0530

    updated README with basic project information
    ADDED:   docs/feature_discussion_1 -> to hold feature 1 requirements and decision log
    
    ADDED pyproject.toml : with config for pytest test discovery and src addition to python path
    ADDED tests/unit/test_orders.py - with failing tests as per requirements in docs/feature_discussion_1
    OTHER Changes:
        RENAMED:    src/main.py -> src/app.py
        RENAMED:    src/test_main.py -> tests/unit/test_app.py

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-
-def adder(a, b):
-    return a + b
-
-
-if __name__ == '__main__':
-    adder(sys.argv[1], sys.argv[2])
---
+++ tests/unit/test_orders.py
@@ -0,0 +1,26 @@
+import pytest
+from datetime import datetime, timedelta
+
+
+def test_batch_allocation_reduces_quantity():
+    pytest.fail('todo')
+
+
+def test_cannot_allocate_when_order_quantity_is_higher_than_available():
+    pytest.fail('todo')
+
+
+def test_can_allocate_when_order_quantity_is_less_than_available():
+    pytest.fail('todo')
+
+
+def test_cannot_allocate_same_order_multiple_times():
+    pytest.fail('todo')
+
+
+def test_prefer_allocation_to_warehouse_over_shipment():
+    pytest.fail('todo')
+
+
+def test_allocation_to_batch_with_minimum_eta():
+    pytest.fail('todo')
