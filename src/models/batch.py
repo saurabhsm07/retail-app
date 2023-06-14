b288c00000480ec597b9dc2e38230e15ef701ebf
maverick <mahambrey.saurabh07@gmail.com>
2023-06-14 18:19:46 +0530

    added deallocate method to remove a perticular order_line allocated to a perticular batch_allocations attribute to keep track of all allocated order lines for a perticular batch

--- src/models/batch.py
+++ src/models/batch.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime
+from typing import List
 
 from models.order_line import OrderLine
 
@@ -10,16 +11,29 @@ class Batch:
     sku: str
     available_quantity: int
     eta: datetime
+    _allocations = set()
 
     def allocate(self, order_line: OrderLine):
         try:
             if self.can_allocate(order_line):
+                self._allocations.add(order_line)
                 self.available_quantity -= order_line.quantity
         except Exception as e:
             raise Exception('Order allocation failed: ' + str(e))
 
-    def can_allocate(self, order_line: OrderLine) -> bool:
-        if order_line.sku == self.sku and order_line.quantity <= self.available_quantity:
+    def deallocate(self, order_line):
+        if order_line in self._allocations:
+            self._allocations.remove(order_line)
+            self.available_quantity += order_line.quantity
             return True
 
         return False
+
+    def can_allocate(self, order_line: OrderLine) -> bool:
+        if order_line.sku == self.sku and order_line.quantity <= self.available_quantity and order_line not in self._allocations:
+            return True
+
+        return False
+
+    def get_allocations(self):
+        return self._allocations
--- src/models/order_line.py
+++ src/models/order_line.py
@@ -6,3 +6,8 @@ class OrderLine:
     order_id: str
     sku: str
     quantity: int
+
+    def __eq__(self, other):
+        if isinstance(other, OrderLine):
+            return True if self.order_id == other.order_id else False
+        return False
