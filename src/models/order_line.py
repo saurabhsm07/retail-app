7f747df8f42e50f4761f729b6235877464f559d1
maverick <mahambrey.saurabh07@gmail.com>
2023-06-20 16:20:21 +0530

    updated OrderLine class as it is a data object and not a unique entity, see feature_disscussion_1 for more information

--- src/models/order_line.py
+++ src/models/order_line.py
@@ -7,7 +7,3 @@ class OrderLine:
     sku: str
     quantity: int
 
-    def __eq__(self, other):
-        if isinstance(other, OrderLine):
-            return True if self.order_id == other.order_id else False
-        return False
