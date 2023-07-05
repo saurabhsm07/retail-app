482dbc51e94e667cb7aa0b3b7a3679ea3c14163a
maverick <mahambrey.saurabh07@gmail.com>
2023-07-05 18:37:00 +0530

    WORKAROUND FIX deleted tables at the start of each unit test to avoid errors caused when we run the complete test suite

--- tests/unit/test_orm.py
+++ tests/unit/test_orm.py
@@ -3,13 +3,14 @@ from models import OrderLine
 
 
 def test_orderline_mapper_can_load_lines(session):  # (1)
+    session.query(OrderLine).delete()
     session.execute(
-        text('INSERT INTO order_lines (order_id, sku, quantity) '
-             'VALUES '
-             '("order1", "RED-CHAIR", 12), '
-             '("order1", "RED-TABLE", 13),'
-             '("order2", "BLUE-LIPSTICK", 14)')
-    )
+            text('INSERT INTO order_lines (order_id, sku, quantity) '
+                 'VALUES '
+                 '("order1", "RED-CHAIR", 12), '
+                 '("order1", "RED-TABLE", 13),'
+                 '("order2", "BLUE-LIPSTICK", 14)')
+        )
     expected = [
         OrderLine("order1", "RED-CHAIR", 12),
         OrderLine("order1", "RED-TABLE", 13),
@@ -19,6 +20,7 @@ def test_orderline_mapper_can_load_lines(session):  # (1)
 
 
 def test_orderline_mapper_can_save_lines(session):
+    session.query(OrderLine).delete()
     new_line = OrderLine("order1", "DECORATIVE-WIDGET", 12)
     session.add(new_line)
     session.commit()
