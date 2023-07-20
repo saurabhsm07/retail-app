1cf5aa1ded2f54b30f861782bdd7a6c1a44a13f3
maverick <mahambrey.saurabh07@gmail.com>
2023-07-20 19:43:36 +0530

    added test orderlines_can_be_allocated_to_batches to test if I manually add records in allocations table which is a bridge table between batches and allocations does the relationship defined holds true or not

--- tests/unit/test_repository.py
+++ tests/unit/test_repository.py
@@ -1,9 +1,35 @@
 import pytest
 from sqlalchemy import text
 
-from repository import BatchRepository
-from models import Batch
+from repository import BatchRepository, OrderLineRepository
+from models import Batch, OrderLine
 from datetime import datetime, timedelta
+from sqlalchemy.orm import sessionmaker
+
+
+def insert_batch_record(session: sessionmaker, batch_ref: str):
+    session.execute(text('insert into batches (reference, sku, quantity) values'
+                         f'("{batch_ref}","chair-1",50)'))
+    session.commit()
+    result = list(session.execute(text('select id from batches where reference=:ref'),
+                                  dict(ref=batch_ref))
+                  )[0][0]
+    return result
+
+
+def insert_order_line_record(session, order_id):
+    session.execute(text('insert into order_lines (sku, quantity, order_id) values'
+                         f'("chair-1",3,"{order_id}")'
+                         ))
+    session.commit()
+    result = list(session.execute(text('select id from order_lines where order_id=:order_id'),
+                                  dict(order_id=order_id)))[0][0]
+    return result
+
+
+def insert_allocation_records(session, batch_id, order_line_id):
+    session.execute(text(f'insert into allocations(batch_id, order_line_id) values({batch_id},{order_line_id})'))
+    session.commit()
 
 
 def test_batch_repo_can_add_batch(session):
@@ -23,8 +49,27 @@ def test_batch_repo_can_add_batch(session):
 
 
 def test_order_line_repo_can_add_line(session):
-    pytest.fail('todo')
+    order_line_obj = OrderLine(sku='chair',
+                               quantity=50,
+                               order_id='4233')
+
+    order_line_session = OrderLineRepository(session=session)
+    order_line_session.add(order_line_obj)
+    session.commit()
+    result = list(session.execute(text('SELECT sku,quantity,order_id FROM order_lines where order_id =:order_id'),
+                                  dict(order_id=order_line_obj.order_id)))
+
+    assert result == [(order_line_obj.sku,
+                       order_line_obj.quantity,
+                       order_line_obj.order_id)]
 
 
 def test_order_lines_can_be_allocated_to_batches(session):
-    pytest.fail('todo')
+    batch_id = insert_batch_record(session, 'batch-a')
+    order_line_id = insert_order_line_record(session, '423')
+    insert_allocation_records(session, batch_id, order_line_id)
+
+    batch_session = BatchRepository(session)
+    batch_obj = batch_session.get('batch-a')
+
+    assert batch_obj._allocations == {OrderLine(sku="chair-1", quantity=3, order_id="423")}
