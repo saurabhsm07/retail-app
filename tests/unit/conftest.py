e968e833a11d2b1096d71afb3ed84b8ead58c163
Saurabh Mahambrey <mahambrey.saurabh07@gmail.com>
2023-09-14 15:50:14 +0530

    added e2e tests for API testing

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, clear_mappers
 from orm import start_mappers, mapper_registry
 
@@ -12,3 +12,38 @@ def session():
     session = sessionmaker(bind=engine)()
     yield session
     clear_mappers()
+
+
+@pytest.fixture()
+def add_stock(session):
+    batches_added = set()
+    skus_added = set()
+
+    def _add_stock(batches):
+        for ref, sku, qty, eta in batches:
+            session.execute(text('insert into batches (reference, sku, quantity, eta) values'
+                                 f'("{ref}","{sku}",{qty},"{eta}")'))
+
+            batches_added.add([session.execute(text('select id from batches where sku=:sku and reference=:ref'),
+                                               dict(sku=sku, ref=ref))[0][0]])
+            skus_added.add([sku])
+
+        session.commit()
+
+    yield _add_stock
+
+    for batch_id in batches_added:
+        session.execute(
+            text("DELETE FROM allocations WHERE batch_id=:batch_id"),
+            dict(batch_id=batch_id),
+        )
+        session.execute(
+            text("DELETE FROM batches WHERE id=:batch_id"), dict(batch_id=batch_id),
+        )
+
+    for sku in skus_added:
+        session.execute(
+            "DELETE FROM order_lines WHERE sku=:sku", dict(sku=sku),
+        )
+
+        session.commit()
--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -3,6 +3,7 @@ import requests
 import uuid
 
 from config import get_api_url
+from models import Batch
 
 
 def random_suffix():
@@ -17,13 +18,48 @@ def random_batch_ref(name=""):
     return f"batch-{name}-{random_suffix()}"
 
 
-def random_orderid(name=""):
+def random_order_id(name=""):
     return f"order-{name}-{random_suffix()}"
 
 
-def test_valid_request_returns_201_and_allocated_batch_reference():
-    requests.post(f'{get_api_url()}/allocate')
+def test_valid_request_returns_201_and_allocated_batch_reference(add_stock):
+    sku_1 = random_sku('1')
+    sku_2 = random_sku('2')
+    early_batch = random_batch_ref('early')
+    batches = [
+        Batch(reference=early_batch, sku=sku_1, quantity=54, eta='2023-01-06'),
+        Batch(reference=random_batch_ref('later'), sku=sku_1, quantity=5, eta='2023-01-12'),
+        Batch(reference=random_batch_ref('b3'), sku=sku_2, quantity=15, eta=None),
+        Batch(reference=random_batch_ref('b4'), sku=sku_2, quantity=15, eta='2023-01-03')
+    ]
+
+    add_stock(batches)
+
+    req = {'order_id': random_order_id(), 'sku': sku_1, 'qty': 4}
+
+    res = requests.post(f'{get_api_url()}/allocate', json=req)
+
+    assert res.status_code == 201
+    assert res.json()['batch_ref'] == early_batch
 
 
 def test_invalid_request_returns_400_and_error_message():
-    pytest.todo('fail')
+    sku_1 = random_sku('1')
+    sku_2 = random_sku('2')
+    invalid_sku = random_sku('INVALID')
+
+    early_batch = random_batch_ref('early')
+
+    batches = [
+        Batch(reference=early_batch, sku=sku_1, quantity=54, eta='2023-01-06'),
+        Batch(reference=random_batch_ref('later'), sku=sku_1, quantity=5, eta='2023-01-12'),
+        Batch(reference=random_batch_ref('b3'), sku=sku_2, quantity=15, eta=None),
+        Batch(reference=random_batch_ref('b4'), sku=sku_2, quantity=15, eta='2023-01-03')
+    ]
+
+    req = {'order_id': random_order_id(), 'sku': invalid_sku, 'qty': 4}
+
+    res = requests.post(f'{get_api_url()}/allocate', json=req)
+
+    assert res.status_code == 400
+    # assert res.json()['batch_ref'] == early_batch
