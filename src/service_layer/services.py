c9eb196975a634a633a3bfe12f0779831d17c95e
Saurabh Mahambrey <mahambrey.saurabh07@gmail.com>
2023-09-27 18:52:34 +0530

    implemented deallocate() service layer method to deallocate an order line allocated to a perticular batch

--- src/service_layer/services.py
+++ src/service_layer/services.py
@@ -4,6 +4,11 @@ from domain.models.order_line import OrderLine
 from domain.models.batch import allocate as allocate_line
 from adapters.repository import AbstractRepository
 
+'''
+UPDATES:
+    -   Service layer deallocate method requires 2 repositories as input (order line, batch) to execute successfully
+'''
+
 
 class InvalidSkuException(Exception):
     pass
@@ -25,3 +30,28 @@ def allocate(order_line: OrderLine, repository: AbstractRepository, session):
         session.commit()
 
         return batch_ref
+
+
+def deallocate(order_id: str, batch_ref: str, batch_repository: AbstractRepository, order_line_repo: AbstractRepository,
+               session):
+    '''
+    :param order_id:
+    :param batch_ref:
+    :param batch_repository: batch repository object to fetch necessary batch from DB
+    :param order_line_repo: order line repository object to fetch order line data from DB
+    :param session:
+    :return:
+    '''
+
+    try:
+
+        batch = batch_repository.get(batch_ref)
+        order_line = order_line_repo.get_by_order_id_and_sku(order_id, batch.sku)
+        status = batch.deallocate(order_line)
+
+        session.commit()
+
+        return status, order_line.quantity
+
+    except Exception as e:
+        raise e
