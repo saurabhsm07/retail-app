57e54e1060d7ead7836b9cad2b3110538bf4f31d
Saurabh Mahambrey <mahambrey.saurabh07@gmail.com>
2023-10-12 13:19:50 +0530

    added TDD test test_uow_can_retrieve_a_batch_and_allocate_to_it for unit of work method implememtation

---
+++ tests/integration/test_uow.py
@@ -0,0 +1,30 @@
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+from service_layer.unit_of_work import SqlAlchemyBatchUnitOfWork
+
+def insert_batch_in_db(session: Session, reference, sku, quantity, eta):
+    session.execute(text('INSERT INTO batches (reference, sku, _purchased_quantity, eta)'
+                         ' VALUES (:ref, :sku, :qty, :eta)'), dict(ref=reference, sku=sku, qty=quantity, eta=eta))
+    session.commit()
+
+
+def get_allocated_batch_ref(session: Session, orderid, sku):
+    [[order_line_id]] = session.execute(text('SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku'),
+                                      dict(orderid=orderid,
+                                           sku=sku)
+                                      )
+    [[batch_ref]] = session.execute(
+        text('SELECT b.reference FROM allocations JOIN batches AS b ON batch_id = b.id'
+             ' WHERE order_line_id=:order_line_id'),
+        dict(order_line_id=order_line_id)
+    )
+    return batch_ref
+
+
+def test_uow_can_retrieve_a_batch_and_allocate_to_it(session_factory):
+    session = session_factory()
+    insert_batch_in_db(session, 'b1', 'workbench', 100, None)
+
+    with SqlAlchemyBatchUnitOfWork() as uow:
+        batches = uow.getBatches()
+        batches.allocate()
