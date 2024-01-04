a70c96f57b9a38cd0836a57c0d57a62294b6a4fb
Saurabh Mahambrey <mahambrey.saurabh07@gmail.com>
2024-01-04 16:53:02 +0530

    UPDATE service layer and tests to use product repo

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -3,41 +3,29 @@ import pytest
 from domain.models.batch import Batch
 from domain.models.order_line import OrderLine
 from adapters.repository import AbstractRepository
+from domain.models.product import Product
+from domain.models.product import allocate as allocate_line
 from service_layer.services import allocate, InvalidSkuException, deallocate, insert_batch
-from service_layer.unit_of_work import AbstractBatchUnitOfWork
+from service_layer.unit_of_work import AbstractUnitOfWork
+from tests.conftest import get_random_sku, get_random_batch_ref
 
 
-class FakeBatchRepository(AbstractRepository):
+class FakeProductRepository(AbstractRepository):
 
-    def __init__(self, batches):
-        self._batches = set(batches)
+    def __init__(self, products):
+        self._products = set(products)
 
-    def add(self, batch: Batch):
-        self._batches.add(batch)
+    def add(self, product):
+        self._products.add(product)
 
-    def get(self, reference) -> Batch:
-        return next(batch for batch in self._batches if batch.reference == reference)
-
-    def list(self):
-        return self._batches
-
-
-class FakeOrderLineRepository(AbstractRepository):
-
-    def __init__(self, line):
-        self._lines = set(line)
-
-    def add(self, line: OrderLine):
-        self._lines.add(line)
-
-    def get(self, order_id) -> OrderLine:
-        return next(line for line in self._lines if line.order_id == order_id)
-
-    def get_by_order_id_and_sku(self, order_id, sku):
-        return next(line for line in self._lines if line.order_id == order_id and line.sku == sku)
+    def get(self, sku):
+        try:
+            return next(product for product in self._products if product.sku == sku)
+        except StopIteration:
+            return None
 
     def list(self):
-        return self._lines
+        return self._products
 
 
 class FakeSession():
@@ -47,10 +35,10 @@ class FakeSession():
         self.committed = True
 
 
-class FakeBatchesUnitOfWork(AbstractBatchUnitOfWork):
+class FakeProductUnitOfWork(AbstractUnitOfWork):
 
     def __init__(self):
-        self.batches = FakeBatchRepository([])
+        self.products = FakeProductRepository([])
         self.committed = False
 
     def commit(self):
@@ -61,67 +49,75 @@ class FakeBatchesUnitOfWork(AbstractBatchUnitOfWork):
 
 
 def test_service_returns_allocated_batch_reference_on_successful_allocation():
-    order_line = OrderLine("o1", "red-lamp", 10)
-    batch = Batch("b1", "red-lamp", 10, None)
+    test_sku = get_random_sku('lamp')
+    test_batch_ref = get_random_batch_ref("batch")
+    order_line = OrderLine("o1", test_sku, 10)
+    product = Product(sku=test_sku, batches=[Batch(test_batch_ref, test_sku, 10, None),
+                                             Batch("b2", test_sku, 5, None)])
 
-    repo = FakeBatchRepository([batch])
+    repo = FakeProductRepository([product])
 
     result = allocate(order_line, repo, FakeSession())
-    assert result == 'b1'
+    assert result == test_batch_ref
 
 
-def test_service_successfully_deallocates_order_line_from_batch():
-    # TODO - we are currently using allocate domain method directly seems to be the right approach.
+def test_service_successfully_deallocates_order_line_from_product():
+    test_sku = get_random_sku('lamp')
+    test_batch_ref = get_random_batch_ref("batch")
 
-    order_line_1 = OrderLine("o1", "red-lamp", 10)
-    order_line_2 = OrderLine("o2", "red-lamp", 10)
-    order_line_3 = OrderLine("deallocate", "red-lamp", 41)
-    batch = Batch("b1", "red-lamp", 100, None)
+    order_line_1 = OrderLine("o1", test_sku, 10)
+    order_line_2 = OrderLine("o2", test_sku, 10)
+    order_line_3 = OrderLine("deallocate", test_sku, 41)
 
-    batch.allocate(order_line_1)
-    batch.allocate(order_line_2)
-    batch.allocate(order_line_3)
+    product = Product(test_sku, [Batch(test_batch_ref, test_sku, 100, None)])
 
-    batch_repo = FakeBatchRepository([batch])
-    order_line_repo = FakeOrderLineRepository([order_line_1, order_line_2, order_line_3])
+    _ = (allocate_line(order_line_1, product),
+         allocate_line(order_line_2, product),
+         allocate_line(order_line_3, product))
 
-    batch = batch_repo.get("b1")
-    curr_allocated_qty = batch.allocated_quantity
-    deallocate(order_line_3.order_id, batch.reference, batch_repo, order_line_repo, FakeSession())
+    product_repo = FakeProductRepository([product])
 
-    assert curr_allocated_qty == batch_repo.get("b1").allocated_quantity + order_line_3.quantity
+    curr_batch = product_repo.get(test_sku).batches[0]
+
+    curr_allocated_qty = curr_batch.allocated_quantity
+    status = deallocate(curr_batch.reference, order_line_3, product_repo, FakeSession())
+
+    assert status
+    assert curr_allocated_qty == product_repo.get(test_sku).batches[0].allocated_quantity + order_line_3.quantity
 
 
 def test_service_raises_error_for_invalid_sku():
-    order_line = OrderLine("o1", "invalid-sku", 10)
-    batch = Batch("b1", "red-lamp", 10, None)
+    valid_sku = get_random_sku('paint')
+    order_line = OrderLine("o1", "invalid_sku", 10)
+    product = Product(valid_sku, [Batch("b1", valid_sku, 10, None)])
 
-    repo = FakeBatchRepository([batch])
+    repo = FakeProductRepository([product])
 
-    with pytest.raises(InvalidSkuException, match='Invalid sku : invalid-sku'):
+    with pytest.raises(InvalidSkuException, match='Invalid sku : invalid_sku'):
         allocate(order_line, repo, FakeSession())
 
 
 def test_service_commits_changes_on_successful_allocation():
-    order_line = OrderLine("o1", "red-lamp", 10)
-    batch = Batch("b1", "red-lamp", 10, None)
+    test_sku = get_random_sku('red-lamp')
+    order_line = OrderLine("o1", test_sku, 10)
+    product = Product(test_sku, [Batch("b1", test_sku, 10, None)])
 
-    repo = FakeBatchRepository([batch])
+    repo = FakeProductRepository([product])
 
     session = FakeSession()
-    result = allocate(order_line, repo, session)
+    _ = allocate(order_line, repo, session)
     assert session.committed
 
 
 def test_service_can_insert_a_batch_on_valid_request():
     batch_attrs = {"reference": "b_a", "sku": "green-lamp", "quantity": 10, "eta": None}
 
-    aow = FakeBatchesUnitOfWork()
+    aow = FakeProductUnitOfWork()
     status = insert_batch(aow, **batch_attrs)
 
     assert status
-    assert aow.batches.get(batch_attrs['reference']) == Batch(reference=batch_attrs['reference'],
-                                                              sku=batch_attrs['sku'],
-                                                              quantity=batch_attrs['quantity'],
-                                                              eta=batch_attrs['eta'])
+    assert aow.products.get(batch_attrs['sku']).batches[0] == Batch(reference=batch_attrs['reference'],
+                                                                    sku=batch_attrs['sku'],
+                                                                    quantity=batch_attrs['quantity'],
+                                                                    eta=batch_attrs['eta'])
     assert aow.committed
