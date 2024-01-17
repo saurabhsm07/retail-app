fcfeb70612a4f4df8bcf1bde05ffa3c03f43b973
maverick <mahambrey.saurabh07@gmail.com>
2024-01-17 13:23:28 +0530

    Moved allocate to Product class Was a miss to not notice that this method naturally within the product class

--- src/domain/models/product.py
+++ src/domain/models/product.py
@@ -14,24 +14,24 @@ class Product:
         self.batches = batches
         self.version = version
 
+    def allocate(self, order_line: OrderLine) -> str:
+        """
+        method selects most suitable batch from a list of product batches for a particular order line,
+        raises OutOfStock exception if order cannot be fulfilled
+        :param order_line:
+        :param product: product that matches a particular SKU
+        :return: batch_ref (batch this particular order line is allocated to)
+        """
+        batches = self.batches
+        most_suitable_batch = None
+        for batch in sorted(batches):
+            if batch.can_allocate(order_line):
+                most_suitable_batch = batch
+                break
 
-def allocate(order_line: OrderLine, product: Product) -> str:
-    """
-    method selects most suitable batch from a list of product batches for a particular order line,
-    raises OutOfStock exception if order cannot be fulfilled
-    :param order_line:
-    :param product: product that matches a particular SKU
-    :return: batch_ref (batch this particular order line is allocated to)
-    """
-    batches = product.batches
-    most_suitable_batch = None
-    for batch in sorted(batches):
-        if batch.can_allocate(order_line):
-            most_suitable_batch = batch
-            break
+        if most_suitable_batch is not None:
+            self.version += 1
+            most_suitable_batch.allocate(order_line)
+            return most_suitable_batch.reference
 
-    if most_suitable_batch is not None:
-        most_suitable_batch.allocate(order_line)
-        return most_suitable_batch.reference
-
-    raise OutOfStockException(f'Product {order_line.sku} of quantity {order_line.quantity} unavailable')
+        raise OutOfStockException(f'Product {order_line.sku} of quantity {order_line.quantity} unavailable')
