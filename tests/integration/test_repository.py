cbcf306d1a70799d374ce216eb04d89397c997ec
maverick <mahambrey.saurabh07@gmail.com>
2024-03-01 18:15:24 +0530

    Updated tests for dual DB type support Tests can now be executed against sql lite as well as postgre_db

--- tests/integration/test_repository.py
+++ tests/integration/test_repository.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 
 from domain.models.product import Product
 from tests.conftest import get_random_sku, get_random_batch_ref, insert_product_record, insert_batch_record, \
-    insert_order_line_record, insert_allocation_records
+    insert_order_line_record, insert_allocation_records, get_random_order_id
 
 """
 TODO: add fixture with inner method to cleanup test data objects
@@ -29,7 +29,8 @@ def test_product_repo_saves_product_batches_to_db(session):
     product_repo.add(p1)
     session.commit()
 
-    batch_result = [x[0] for x in list(session.execute(text(f'SELECT reference FROM batches WHERE sku="{test_sku}"')))]
+    batch_result = [x[0] for x in
+                    list(session.execute(text(f'SELECT reference FROM batches WHERE sku = \'{test_sku}\'')))]
 
     assert len(batch_result) == len(batches)
     for batch in batches:
@@ -39,34 +40,37 @@ def test_product_repo_saves_product_batches_to_db(session):
 def test_order_lines_can_be_allocated_to_batches(session):
     test_sku = get_random_sku('car')
     test_batch_ref = get_random_batch_ref('batch-a')
+    test_order_id = get_random_order_id('o1')
     _ = insert_product_record(session, sku=test_sku)
     batch_id = insert_batch_record(session, test_batch_ref, test_sku, 50)
-    order_line_id = insert_order_line_record(session, '423', test_sku, 4)
+    order_line_id = insert_order_line_record(session, test_order_id, test_sku, 4)
     insert_allocation_records(session, batch_id, order_line_id)
 
     product_repo = ProductRepository(session)
     batch_obj = list(filter(lambda batch: batch.reference == test_batch_ref, product_repo.get(test_sku).batches))[0]
-    assert batch_obj._allocations == {OrderLine(sku=test_sku, quantity=4, order_id="423")}
+    assert batch_obj._allocations == {OrderLine(sku=test_sku, quantity=4, order_id=test_order_id)}
 
 
 def test_order_line_can_be_de_allocated_from_batches(session):
     test_sku = get_random_sku('car')
     test_batch_ref = get_random_batch_ref('batch-a')
+    alloc_order_id = get_random_order_id('allocated')
+    dealloc_order_id = get_random_order_id('deallocated')
     _ = insert_product_record(session, sku=test_sku)
     batch_id = insert_batch_record(session, test_batch_ref, test_sku, 50)
-    order_line_id_1 = insert_order_line_record(session, 'deallocated', test_sku, 4)
-    order_line_id_2 = insert_order_line_record(session, 'allocated', test_sku, 4)
+    order_line_id_1 = insert_order_line_record(session, dealloc_order_id, test_sku, 4)
+    order_line_id_2 = insert_order_line_record(session, alloc_order_id, test_sku, 4)
     insert_allocation_records(session, batch_id, order_line_id_1)
     insert_allocation_records(session, batch_id, order_line_id_2)
 
     product_repo = ProductRepository(session)
     batch_obj = list(filter(lambda batch: batch.reference == test_batch_ref, product_repo.get(test_sku).batches))[0]
 
-    batch_obj.deallocate(OrderLine(sku=test_sku, quantity=4, order_id="deallocated"))
+    batch_obj.deallocate(OrderLine(sku=test_sku, quantity=4, order_id=dealloc_order_id))
 
     session.commit()  # committing the session to see if updating repo object reflects the change in DB or not
 
     batch_obj = list(filter(lambda batch: batch.reference == test_batch_ref, product_repo.get(test_sku).batches))[0]
 
-    assert (OrderLine(sku=test_sku, quantity=4, order_id="deallocated") not in batch_obj._allocations and
-            OrderLine(sku=test_sku, quantity=4, order_id="allocated") in batch_obj._allocations)
+    assert (OrderLine(sku=test_sku, quantity=4, order_id=dealloc_order_id) not in batch_obj._allocations and
+            OrderLine(sku=test_sku, quantity=4, order_id=alloc_order_id) in batch_obj._allocations)
